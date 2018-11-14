6ad8507a39de7d06fe52eda0dcbac896b710bad4
AmirAhmedDarrag <amirdarrag2013@gmail.com>
2018-11-14 10:10:42 +0200

    AddApp First commit
    Co-Authored-By: Amir Ahmed <amirdarrag2013@gmail.com>

---
+++ Assets/Buttons.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour {
+
+    public GameObject[] _Obj;
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < _Obj.Length; i++)
+        {
+            _Obj[i].SetActive(false);
+        }
+	}
+	
+	public void AppearedObject(int inx)
+    {
+       
+        for (int i = 0; i < _Obj.Length; i++)
+        {
+            if (i == inx)
+            {
+                _Obj[i].SetActive(true);
+            }
+            else
+            {
+                _Obj[i].SetActive(false);
+            }
+        }
+    }
+}
